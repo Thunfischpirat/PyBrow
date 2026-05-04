499b8492914fad4deb84d0d33738fc612c5e1124
Thunfischpirat <christiansinger09@gmail.com>
2026-05-04 18:31:32 +0200

    feat: support content-length header

--- browser.py
+++ browser.py
@@ -2,6 +2,8 @@ import ssl
 import socket
 from urllib.parse import unquote
 
+OPEN_SOCKETS = {}
+
 
 class URL:
     def __init__(self, url: str) -> None:
@@ -36,44 +38,51 @@ class URL:
             self.port = int(port)
 
     def request_http(self) -> str:
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_STREAM,
-            proto=socket.IPPROTO_TCP,
-        )
-        s.connect((self.host, self.port))
+        s = OPEN_SOCKETS.get(f"{self.scheme}-{self.host}-{self.port}")
 
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
+        if s is None:
+            s = socket.socket(
+                family=socket.AF_INET,
+                type=socket.SOCK_STREAM,
+                proto=socket.IPPROTO_TCP,
+            )
+            s.connect((self.host, self.port))
+
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
+
+            OPEN_SOCKETS[f"{self.scheme}-{self.host}-{self.port}"] = s
 
         headers = [
             f"GET {self.path} HTTP/1.1",
             f"Host: {self.host}",
-            "Connection: close",
-            "User-Agent: PyBrow",
+             "Connection: keep-alive",
+             "User-Agent: PyBrow",
         ]
 
         request = "\r\n".join(headers) + "\r\n\r\n"
 
         s.sendall(request.encode("utf8"))
 
-        response = s.makefile("r", encoding="utf8", newline="\r\n")
+        response = s.makefile("rb", encoding="utf8", newline="\r\n")
         statusline = response.readline()
-        version, status, explanation = statusline.split(" ", 2)
+        version, status, explanation = statusline.decode("utf-8").split(" ", 2)
 
         response_headers = {}
         while True:
-            line = response.readline()
+            line = response.readline().decode("utf-8")
             if line == "\r\n":
                 break
             header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
 
         assert "content-encoding" not in response_headers
+        assert "transfer-encoding" not in response_headers
 
-        content = response.read()
-        s.close()
+        num_bytes = int(response_headers["content-length"])
+        raw = response.read(num_bytes)
+        content = raw.decode("utf-8")
 
         return content
 
